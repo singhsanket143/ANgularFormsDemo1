efe1333f2d98bc1d1386890a96185901da789377
Sanket  Singh <sanketsingh@Sankets-MacBook-Air.local>
2017-12-29 21:26:37 +0530

    keystroke update added with 2 way binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,8 @@ import {NgForm} from '@angular/forms';
 })
 export class AppComponent {
   @ViewChild('f') signUpForm = NgForm;
+  defaultQuestion = 'pet';
+  answer = '';
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
